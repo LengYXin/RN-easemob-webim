810646d612c205a3c4fbc307d93bf8cee89f19bf
leng <896630361@qq.com>
2018-01-15 13:16:18 +0800

    创建 51 版本rn

--- newApp/android/app/src/main/java/com/newapp/MainActivity.java
+++ newApp/android/app/src/main/java/com/newapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.newapp;
 
-import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+// public class MainActivity extends ReactActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "newApp";
-    }
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "newApp";
+//     }
+// }
+
+import com.reactnativenavigation.controllers.SplashActivity;
+
+public class MainActivity extends SplashActivity {
+
 }
